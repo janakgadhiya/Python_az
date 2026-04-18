8f1d9147030b6f09e70a0007cb18f96b6f97c6d8
janakgadhiya <145697966+janakgadhiya@users.noreply.github.com>
2026-04-18 15:35:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,9 +8,15 @@ def home():
 
 @app.route('/add')
 def add():
-    a = int(request.args.get('a', 0))
-    b = int(request.args.get('b', 0))
+    a = int(request.args.get('a', 10))
+    b = int(request.args.get('b', 5))
     return {"result": a + b}
+    
+@app.route('/sub')
+def sub():
+    a = int(request.args.get('a', 6))
+    b = int(request.args.get('b', 3))
+    return {"result": a - b}
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
